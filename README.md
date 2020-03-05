e05e958fd0cefd9c6cca825c5bf3a2f8e5bc3991
Tyler Murry <tyman7@gmail.com>
2020-03-04 21:29:41 -0600

    did it

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # decode-har
-Decodes responses inside of a HAR file for easily manipulation. Purpose built to assist with PollyJS captures.
+This is an ultra-light command line utility that decodes the responses of a HAR file.
+
+Specifically, this was built to make manipulating PollyJS captures easier as it saves the encoded and hex-chunked output which is hard to work with. 
+
+## Installing
+`npm install -g decode-har`
+
+## Usage
+`decodehar your_har_file.har`
+
+This will create a new file called `your_har_file.har.decoded`
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const { ungzip } = require('node-gzip');
+
+function mergeChunks (chunks) {
+  if (!chunks || chunks.length === 0) { return Buffer.alloc(0) }
+  if (!Buffer.isBuffer(chunks[0])) { return chunks.join('') }
+
+  return Buffer.concat(chunks);
+}
+
+function getContentEncodingHeader(headers) {
+  return headers.filter(h => h.name === 'content-encoding')[0];
+}
+
+function getContentEncoding(headers) {
+  const header = getContentEncodingHeader(headers);
+  return header ? header.value : null;
+}
+
+function isUtf8Representable(buffer) {
+  return Buffer.from(buffer.toString('utf8'), 'utf8').equals(buffer)
+}
+
+function getChunksFromBody(body, headers) {
+  if (!body) { return [] }
+  if (Buffer.isBuffer(body)) { return [body] }
+
+  if (getContentEncoding(headers)) {
+    return JSON.parse(body)
+      .map(chunk => Buffer.from(chunk, 'hex'));
+  }
+
+  const buffer = Buffer.from(body);
+
+  return [Buffer.from(buffer, isUtf8Representable(buffer) ? 'utf8' : 'hex')];
+}
+
+const run = async () => {
+  const harFile = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
+  for (const [index, entry] of harFile.log.entries.entries()) {
+    const headers = entry.response.headers;
+    let content = getChunksFromBody(entry.response.content.text, headers);
+
+    if (getContentEncoding(headers) === 'gzip') {
+      content = await ungzip(mergeChunks(content));
+
+      headers.splice(headers.indexOf(getContentEncodingHeader(headers)), 1);
+    }
+
+    harFile.log.entries[index].response.content.text = content.toString('utf8');
+  }
+
+  fs.writeFileSync('recording.har.decoded', JSON.stringify(harFile, null, 2), 'utf8');
+
+  console.log('Successfully decoded file to recording.har.decoded');
+};
+
+run();
